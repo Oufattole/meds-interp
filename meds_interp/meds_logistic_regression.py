c8c13f92bc76b134e6a63b38c91c778697d3a617
Nassim <noufattole@gmail.com>
2024-06-24 12:11:51 -0400

    added boilerplate code for train + test split and evaluating metrics

--- meds_interp/meds_logistic_regression.py
+++ meds_interp/meds_logistic_regression.py
@@ -2,14 +2,16 @@ import polars as pl
 from sklearn.linear_model import LogisticRegression
 import numpy as np
 
-def fit_logistic_regression(df: pl.DataFrame, c) -> pl.Series:
-    """
+def fit_logistic_regression(train_df: pl.DataFrame, test_df: pl.DataFrame, c) -> pl.Series:
+    """Fits Logistic Regression model to training dataset and gets labels on test set.
     https://scikit-learn.org/stable/modules/linear_model.html#logistic-regression
 
     Args:
-        df (pl.DataFrame): input data, column `embeddings` has embeddings that you feed to logistic regression
+        train_df (pl.DataFrame): input data to fit logistic regression on, column
+            `embeddings` has embeddings, column `label` has labels
+        test_df (pl.DataFrame): data to generate predictions for. Has an `embeddings` column.
     Returns:
-        pl.Series: labels from logistic regression model
+        pl.Series: labels from logistic regression model for the test_df embeddings
     """
     # embeddings = df.get_column("embeddings")
     # labels = df.get_column("labels")
@@ -26,4 +28,15 @@ def fit_logistic_regression(df: pl.DataFrame, c) -> pl.Series:
     predicted_labels = classifier.predict(embeddings)
 
     return pl.Series(predicted_labels)
-    
+
+def score_labels(test_df: pl.DataFrame):
+    """Evaluates predictions
+    Args:
+        test_df: has column "prediction" with model predictions and "label" with ground truth labels
+    Returns:
+        dictionary of different evaluation metrics.
+    """
+    return dict(
+        auc=0,
+        accuracy=0,
+    )
