0c3f238fbc969578fb463f0b6937bb5c31f5cda3
Nassim <noufattole@gmail.com>
2024-07-01 17:18:18 -0400

    added gitignore and started adding knn optimizer support

---
+++ src/meds_interp/knn_raw_code.py
@@ -0,0 +1,244 @@
+import os
+
+import faiss
+import rootutils
+
+root = rootutils.setup_root(os.path.abspath(""), dotenv=True, pythonpath=True, cwd=True)
+import os
+from enum import Enum
+
+import numpy as np
+import polars as pl
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.metrics import average_precision_score, log_loss, roc_auc_score
+from sklearn.neighbors._base import _get_weights
+from sklearn.preprocessing import Normalizer
+from sklearn.utils.multiclass import unique_labels
+
+
+def get_results(ground_truth, pred, pred_proba):
+    logits = pred_proba[:, 1]
+    result = dict(
+        auc=roc_auc_score(ground_truth, logits) * 100,
+        apr=average_precision_score(ground_truth, logits) * 100,
+        acc=np.mean((ground_truth == pred).astype(float)) * 100.0,
+        loss=log_loss(ground_truth, logits),
+    )
+    return result
+
+
+class Preprocess_Type(Enum):
+    NONE = "NONE"
+    NORM_SEPERATLY = "NORM_SEPERATLY"
+    NORM_AFTER_CONCAT = "NORM_AFTER_CONCAT"
+
+
+class DualCombo(Enum):
+    AVG = "AVG"
+    CONCAT = "CONCAT"
+
+
+class KNN_Model(BaseEstimator, ClassifierMixin):
+    def __init__(
+        self,
+        modalities: list[str],
+        modality_weights: list[float],
+        n_neighbors=5,
+        n_jobs=None,
+        algorithm="l2",
+        weights="uniform",
+        preprocess: Preprocess_Type = Preprocess_Type.NONE,
+        C=2.0,
+        d=32,
+    ):
+        self.modalities = modalities
+        self.modality_weights = modality_weights
+        self.n_neighbors = n_neighbors
+        self.n_jobs = n_jobs
+        self.algorithm = algorithm
+        self.weights = weights
+        self.preprocess = preprocess
+        self.C = C
+        self.d = d
+
+    def predict(self, X):
+        proba = self.predict_proba(X)
+        return np.argmax(proba, axis=1)
+
+    def kneighbors(self, X: np.array, n_neighbors=None, return_distance=True):
+        if n_neighbors is None:
+            n_neighbors = self.n_neighbors
+
+        elif n_neighbors <= 0:
+            raise ValueError("Expected n_neighbors > 0." " Got {}".format(n_neighbors))
+        else:
+            if not np.issubdtype(type(n_neighbors), np.integer):
+                raise TypeError(
+                    "n_neighbors does not take {} value, " "enter integer value".format(type(n_neighbors))
+                )
+
+        X = np.atleast_2d(X).astype(np.float32)
+        dist, idx = self.index.search(X, n_neighbors)
+
+        if return_distance:
+            return dist, idx
+        else:
+            return idx
+
+    def get_uniform_proba(self, idx):
+        class_idx = self.y_[idx]
+        counts = np.apply_along_axis(
+            lambda x: np.bincount(x, minlength=self.n_classes_), axis=1, arr=class_idx.astype(np.int16)
+        )
+        return counts / self.n_neighbors
+
+    def get_distance_proba(self, neigh_dist, neigh_ind):
+        _y = self.y_
+        # _y = _y.reshape((-1, 1))
+        n_queries = neigh_dist.shape[0]
+
+        weights = _get_weights(neigh_dist, self.weights)
+        if weights is None:
+            weights = np.ones_like(neigh_ind)
+
+        all_rows = np.arange(n_queries)
+        # probabilities = []
+
+        classes = self.classes_
+        pred_labels = _y[neigh_ind]
+        proba = np.zeros((n_queries, classes.size))
+
+        # a simple ':' index doesn't work right
+        for i, idx in enumerate(pred_labels.T):  # loop is O(n_neighbors)
+            proba[all_rows, idx] += weights[:, i]
+
+        # normalize 'votes' into real [0,1] probabilities
+        normalizer = proba.sum(axis=1)[:, np.newaxis]
+        proba /= normalizer
+        return proba
+
+    def predict_proba(self, X):
+        """Estimates the posterior probabilities for sample in X.
+
+        Parameters
+        ----------
+        X : array of shape (n_samples, n_features)
+            The input data.
+
+        Returns
+        -------
+        preds_proba : array of shape (n_samples, n_classes)
+                          Probabilities estimates for each sample in X.
+        """
+        X = self.transform_preprocess(X)
+        if self.weights == "uniform":  # uses closest neighbors and treats all labels equally
+            idx = self.kneighbors(X, self.n_neighbors, return_distance=False)
+            return self.get_uniform_proba(idx)
+        else:  # weights neighbor's labels by inverse distance to the neighbor
+            assert self.weights == "distance"
+            neigh_dist, neigh_ind = self.kneighbors(X, self.n_neighbors, return_distance=True)
+            return self.get_distance_proba(neigh_dist, neigh_ind)
+
+    def concat_data(self, X: list[np.array] | pl.DataFrame):
+        embeddings = []
+        if isinstance(X, pl.DataFrame):
+            for modality in self.modalities:
+                embed_array = np.asarray(X[modality].to_list())
+                embeddings.append(embed_array)
+        else:
+            embeddings = X
+        return np.concatenate(embeddings, axis=1)
+
+    def fit_preprocess(self, X):
+        self.get_modality_lengths(X)
+        if self.preprocess == Preprocess_Type.NORM_AFTER_CONCAT:
+            X = self.concat_data(X)
+            self.scaler = Normalizer()
+            self.scaler.fit(X)
+        elif self.preprocess == Preprocess_Type.NORM_SEPERATLY:
+            self.scalers = []
+            assert len(self.modalities) == len(self.modality_weights)
+            for modality in self.modalities:
+                embed_array = np.asarray(X[modality].to_list())
+                scaler = Normalizer()
+                scaler.fit(embed_array)
+                self.scalers.append(scaler)
+        else:
+            assert self.preprocess == Preprocess_Type.NONE
+
+    def get_modality_lengths(self, X: pl.DataFrame):
+        modality_lengths = []
+        for modality in self.modalities:
+            modality_lengths.append(X.get_column(modality)[0].len())
+        self.modality_lengths = modality_lengths
+
+    def transform_preprocess(self, X: pl.DataFrame):
+        reweight_vector = np.repeat(self.modality_weights, self.modality_lengths)[None, :]
+        if self.preprocess == Preprocess_Type.NORM_AFTER_CONCAT:
+            X = self.concat_data(X)
+            self.scaler = Normalizer()
+            X = self.scaler.transform(X)
+            # return X
+        elif self.preprocess == Preprocess_Type.NORM_SEPERATLY:
+            self.scalers = []
+            transformed_x = []
+            for modality in self.modalities:
+                embed_array = np.asarray(X[modality].to_list())
+                scaler = Normalizer()
+                transformed_x.append(scaler.transform(embed_array))
+            X = self.concat_data(transformed_x)
+        else:
+            assert self.preprocess == Preprocess_Type.NONE
+            X = self.concat_data(X)
+
+        return X * reweight_vector
+
+    def fit(self, X, y):
+        """Fit the model according to the given training data.
+
+        Parameters
+        ----------
+        X : array of shape (n_samples, n_features)
+            Data used to fit the model.
+
+        y : array of shape (n_samples)
+            class labels of each example in X.
+        """
+        self.classes_ = unique_labels(y)
+        self.fit_preprocess(X)
+        X = self.transform_preprocess(X)
+        d = X.shape[1]  # dimensionality of the feature vector
+        self._prepare_knn_algorithm(X, d, False)
+        self.index.add(X)
+        self.y_ = np.array(y)
+        self.n_classes_ = np.unique(y).size
+        return self
+
+    def _prepare_knn_algorithm(self, X, d, gpu_usage=False):
+        """_summary_
+
+        Args:
+            X (_type_): _description_
+            d (_type_): dimension of the embeddings
+            gpu_usage (boolean): boolean for whether or not gpu should be used
+
+        Raises:
+            ValueError: _description_
+        """
+        if self.algorithm == "l2":
+            index = faiss.IndexFlatL2(sum(self.modality_lengths))
+            self.index = index
+            if gpu_usage:
+                self.index = faiss.index_cpu_to_gpu(faiss.StandardGpuResources(), 0, index)
+        elif self.algorithm == "ip":
+            index = faiss.IndexFlatIP(d)
+            self.index = index
+            if gpu_usage:
+                index = faiss.index_cpu_to_gpu(faiss.StandardGpuResources(), 0, index)
+        else:
+            raise ValueError(
+                "Invalid algorithm option." " Expected ['l2', 'ip'], " "got {}".format(self.algorithm)
+            )
+
+    def decision_function(self, X):
+        return self.predict_proba(X)[:, 1]
---
+++ src/meds_interp/meds_logistic_regression.py
@@ -0,0 +1,48 @@
+import numpy as np
+import polars as pl
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
+
+
+def fit_logistic_regression(train_df: pl.DataFrame, test_df: pl.DataFrame, c) -> pl.Series:
+    """Fits Logistic Regression model to training dataset and gets labels on test set.
+    https://scikit-learn.org/stable/modules/linear_model.html#logistic-regression
+
+    Args:
+        train_df (pl.DataFrame): input data to fit logistic regression on, column
+            `embeddings` has embeddings, column `label` has labels
+        test_df (pl.DataFrame): data to generate predictions for. Has an `embeddings` column.
+    Returns:
+        pl.Series: labels from logistic regression model for the test_df embeddings
+    """
+
+    train_embeddings = np.asarray(train_df["embeddings"].to_list())
+    labels = train_df["label"].to_numpy()
+
+    test_embeddings = np.asarray(test_df["embeddings"].to_list())
+
+    classifier = LogisticRegression(C=c)
+    classifier.fit(train_embeddings, labels)
+    predicted_probabilities = classifier.predict_proba(test_embeddings)
+
+    return pl.Series(predicted_probabilities)
+
+
+def score_labels(test_df: pl.DataFrame):
+    """Evaluates predictions
+    Args:
+        test_df: has column "prediction" with model predictions and "label" with ground truth labels
+    Returns:
+        dictionary of different evaluation metrics.
+    """
+    true_labels = test_df["label"]
+
+    predicted_labels = test_df.get_column("probabilities").list.arg_max()
+    predicted_probs = test_df.get_column("probabilities").list.get(0)
+
+    accuracy = metrics.accuracy_score(true_labels, predicted_labels)
+    auc_score = metrics.roc_auc_score(true_labels, predicted_probs)
+    return dict(
+        auc=float(auc_score),
+        accuracy=accuracy,
+    )
