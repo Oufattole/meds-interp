0532c7700cd433c6bf73b49e35887a4171990734
leandergo <lgoessling@ucsd.edu>
2024-07-03 15:33:50 -0400

    [WIP] adding tabularization of irregularly sampled embeddings using meds-tab

---
+++ test.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# bash test.sh
+set -e
+
+meds-tab-describe MEDS_cohort_dir="/tmp/pytest-of-leander/pytest-23/test_knn_tuning0"
+
+meds-tab-tabularize-static MEDS_cohort_dir="/tmp/pytest-of-leander/pytest-23/test_knn_tuning0" \
+                            tabularization.min_code_inclusion_frequency=10 \
+                            tabularization.window_sizes=[1d,30d,365d,full] \
+                            do_overwrite=False \
+                            tabularization.aggs=[static/present,static/first,code/count,value/count,value/sum,value/sum_sqd,value/min,value/max]
+
+meds-tab-cache-task MEDS_cohort_dir="/tmp/pytest-of-leander/pytest-23/test_knn_tuning0" \
+   task_name=$TASK \
+   tabularization.min_code_inclusion_frequency=10 \
+   do_overwrite=False \
+   tabularization.window_sizes=[1d,30d,365d,full] \
+   tabularization.aggs=[static/present,static/first,code/count,value/count,value/sum,value/sum_sqd,value/min,value/max]
+
+meds-tab-xgboost --multirun \
+   MEDS_cohort_dir="/tmp/pytest-of-leander/pytest-23/test_knn_tuning0" \
+   task_name=$TASK \
+   output_dir="" \
+   tabularization.min_code_inclusion_frequency=10 \
+   tabularization.window_sizes=$(generate-permutations [1d,30d,365d,full]) \
+   do_overwrite=False \
+   tabularization.aggs=$(generate-permutations [static/present,static/first,code/count,value/count,value/sum,value/sum_sqd,value/min,value/max])
