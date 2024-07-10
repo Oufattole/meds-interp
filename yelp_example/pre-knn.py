879cdbf10debe9554468d7f7848719da5976f94d
leandergo <lgoessling@ucsd.edu>
2024-07-09 21:26:31 -0400

    still working on yelp, got review, photo, and caption dfs finished

--- yelp_example/pre-knn.py
+++ yelp_example/pre-knn.py
@@ -1,9 +1,10 @@
 import json
+import os
 
 import numpy as np
 import polars as pl
 import torch
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 from torch.utils.data import DataLoader, Dataset
 from tqdm.auto import tqdm
 from transformers import CLIPModel, CLIPProcessor
@@ -12,6 +13,10 @@ model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 tokenizer = processor.tokenizer
 image_processor = processor.image_processor
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+assert device == "cuda"
+model.to(device)
 
 filepaths = [
     "/storage/shared/yelp_dataset/raw/yelp_academic_dataset_business.json",
@@ -23,19 +28,25 @@ filepaths = [
 
 with open("/storage/shared/yelp_dataset_image/photos.json") as r:
     photo_ids = []
+    photo_business_ids = []
+    photo_labels = []
     captions = []
-    labels = []
-    valid_business_ids = []
+    caption_labels = []
+    caption_business_ids = []
     for line in r:
         data = json.loads(line)
-        photo_ids.append(data["photo_id"])
-        if data["caption"]:
-            captions.append(data["caption"])
-            labels.append(data["label"])
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-assert device == "cuda"
-model.to(device)
+        try:
+            img_path = "/storage/shared/yelp_dataset_image" + "/photos/" + data["photo_id"] + ".jpg"
+            image = Image.open(img_path)
+            photo_ids.append(data["photo_id"])
+            photo_business_ids.append(data["business_id"])
+            photo_labels.append(data["label"])
+            if data["caption"]:
+                captions.append(data["caption"])
+                caption_labels.append(data["label"])
+                caption_business_ids.append(data["business_id"])
+        except UnidentifiedImageError:
+            continue
 
 
 class PhotoDataset(Dataset):
@@ -57,15 +68,18 @@ class PhotoDataset(Dataset):
         return image_tensor
 
 
-photo_ids_list = photo_ids
-photo_dataloader = DataLoader(PhotoDataset(photo_ids_list), shuffle=False, batch_size=64, num_workers=8)
-photo_all_embeddings = []
-for batch in tqdm(photo_dataloader):
-    batch.to(device)
-    with torch.no_grad():
-        embeddings = model.get_image_features(pixel_values=batch["pixel_values"].squeeze(dim=1))
-        photo_all_embeddings.append(embeddings.cpu().numpy())
-photo_stacked_embeddings = np.vstack(photo_all_embeddings)
+photo_output_path = "photos.npy"
+if not os.path.exists(photo_output_path):
+    photo_ids_list = photo_ids
+    photo_dataloader = DataLoader(PhotoDataset(photo_ids_list), shuffle=False, batch_size=64, num_workers=8)
+    photo_all_embeddings = []
+    for batch in tqdm(photo_dataloader):
+        batch.to(device)
+        with torch.no_grad():
+            embeddings = model.get_image_features(pixel_values=batch["pixel_values"].squeeze(dim=1))
+            photo_all_embeddings.append(embeddings.cpu().numpy())
+    photo_stacked_embeddings = np.vstack(photo_all_embeddings)
+    np.save(photo_output_path, photo_stacked_embeddings)
 
 
 class CaptionDataset(Dataset):
@@ -84,21 +98,120 @@ class CaptionDataset(Dataset):
         return self.photo_captions_list[idx]
 
 
-captions_dataloader = DataLoader(CaptionDataset(captions, shuffle=False, batch_size=64))
-caption_all_embeddings = []
-for batch in captions_dataloader:
-    batch.to(device)
-    with torch.no_grad():
-        embeddings = model.get_text_features(batch)
-        caption_all_embeddings.append(embeddings.cpu().to_numpy())
-caption_stacked_embeddings = np.vstack(caption_all_embeddings)
+caption_output_path = "caption.npy"
+if not os.path.exists(caption_output_path):
+    captions_dataloader = DataLoader(CaptionDataset(captions), shuffle=False, batch_size=64)
+    caption_all_embeddings = []
+    for batch in captions_dataloader:
+        tokens = tokenizer(batch, max_length=77, padding="max_length", truncation=True, return_tensors="pt")
+        tokens = {key: value.to(model.device) for key, value in tokens.items()}
 
+        with torch.no_grad():
+            embeddings = model.get_text_features(**tokens)
+            caption_all_embeddings.append(embeddings.cpu().numpy())
+    caption_stacked_embeddings = np.vstack(caption_all_embeddings)
+    np.save(caption_output_path, caption_stacked_embeddings)
+
+# Dataframe of the caption embeddings and the labels
 photo_caption_table = {
-    "business_ids": valid_business_ids,
-    "captions": caption_stacked_embeddings,
-    "labels": labels,
+    "business_ids": caption_business_ids,
+    "captions": np.load("caption.npy"),
+    "labels": caption_labels,
 }
 photo_caption_table = pl.DataFrame(photo_caption_table)
+unique_caption_labels = photo_caption_table["labels"].unique().to_list()
+for label in unique_caption_labels:
+    photo_caption_table = photo_caption_table.with_columns(
+        pl.when(pl.col("labels") == label)
+        .then(pl.col("captions"))
+        .otherwise(None)
+        .alias(f"embeddings_{label}")
+    )
+
+# Dataframe of the image embeddings and the labels
+photo_table = {
+    "business_ids": photo_business_ids,
+    "image_embeddings": np.load("photos.npy"),
+    "labels": photo_labels,
+}
+photo_table = pl.DataFrame(photo_table)
+unique_photo_labels = photo_table["labels"].unique().to_list()
+for label in unique_photo_labels:
+    photo_table = photo_table.with_columns(
+        pl.when(pl.col("labels") == label)
+        .then(pl.col("image_embeddings"))
+        .otherwise(None)
+        .alias(f"embeddings_{label}")
+    )
+# print(photo_table)
+
+# Load all of the reviews into a dataframe
+with open("/storage/shared/yelp_dataset/raw/yelp_academic_dataset_review.json") as r:
+    review_user = []
+    review = []
+    review_business = []
+    timestamps = []
+    useful_count = []
+    funny_count = []
+    cool_count = []
+    stars = []
+    for line in r:
+        data = json.loads(line)
+
+        review.append(data["text"])
+        review_user.append(data["user_id"])
+        review_business.append(data["business_id"])
+        timestamps.append(data["date"])
+        useful_count.append(data["useful"])
+        funny_count.append(data["funny"])
+        cool_count.append(data["cool"])
+        stars.append(data["stars"])
+
+
+def process_review():
+    class ReviewDataset(Dataset):
+        def __init__(self, reviews_list: list[str]):
+            """Dataset of the reviews.
+
+            Args:
+                reviews_list (list[str]): list of all the reviews
+            """
+            self.reviews_list = reviews_list
+
+        def __len__(self):
+            return len(self.reviews_list)
+
+        def __getitem__(self, idx):
+            return self.reviews_list[idx]
+
+    review_output_path = "reviews.npy"
+    model.eval()
+    if not os.path.exists(review_output_path):
+        review_dataloader = DataLoader(ReviewDataset(review), shuffle=False, batch_size=1024)
+        review_all_embeddings = []
+        for batch in tqdm(review_dataloader):
+            tokens = tokenizer(
+                batch, max_length=77, padding="max_length", truncation=True, return_tensors="pt"
+            )
+            tokens = {key: value.to(model.device) for key, value in tokens.items()}
+
+            with torch.no_grad():
+                embeddings = model.get_text_features(**tokens)
+                review_all_embeddings.append(embeddings.cpu().numpy())
+        review_stacked_embeddings = np.vstack(review_all_embeddings)
+        np.save(review_output_path, review_stacked_embeddings)
+
+    review_table = {
+        "user_id": review_user,
+        "timestamp": np.array(timestamps, dtype="datetime64"),
+        "business_ids": review_business,
+        "review_embeddings": np.load("reviews.npy"),
+        "useful": useful_count,
+        "funny": funny_count,
+        "cool": cool_count,
+        "stars": stars,
+    }
+    review_table = pl.DataFrame(review_table)
 
 
 # dataframes = []
